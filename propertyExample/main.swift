21b29a7829a097068121e3fd819545181708c52f
bhadresh-goswami <tops.bhadresh@gmail.com>
2019-01-05 18:49:30 +0530

    Initial Commit

---
+++ propertyExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  propertyExample
+//
+//  Created by Mac on 05/01/19.
+//  Copyright © 2019 Bhadresh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
